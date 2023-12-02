fb0a2a47d7f4482ec73212f972a3b687b8611b7b
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-12-01 21:03:57 -0500

    Update zoneEditorServer.lua

--- Contents/mods/main/media/lua/server/zoneEditorServer.lua
+++ Contents/mods/main/media/lua/server/zoneEditorServer.lua
@@ -19,6 +19,7 @@ function zoneEditorServer.onClientCommand(_module, _command, _player, _data)
     if _command == "addZoneTypeToServer" then
         zoneEditorServer.zoneTypes[_data.zoneType] = true
         if not ModData.exists(_data.zoneType.."_zones") then ModData.getOrCreate(_data.zoneType.."_zones") end
+        ModData.transmit(_data.zoneType.."_zones", {})
     end
 end
 --sendClientCommand("zoneEditor", "addZoneTypeToServer", {zoneType=""})

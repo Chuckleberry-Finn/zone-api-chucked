6003e6018aca5fea2ec07d2eb84d6aa048290570
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-12-02 19:34:09 -0500

    Update zoneEditorServerEvents.lua

--- Contents/mods/main/media/lua/server/zoneEditorServerEvents.lua
+++ Contents/mods/main/media/lua/server/zoneEditorServerEvents.lua
@@ -1,3 +1,3 @@
-local zoneEditorServer = require("zoneEditorServer")
+local zoneEditorServer = require "zoneEditorServer"
 Events.OnReceiveGlobalModData.Add(zoneEditorServer.receiveGlobalModData)
 Events.OnClientCommand.Add(zoneEditorServer.onClientCommand)--/client/ to server
